c2ce3770aeb979a3a987d22191978848b1a94998
ZubanAlex <zubanalex31@gmail.com>
2020-03-27 21:08:20 +0200

    Updates

---
+++ precache-manifest.bae0509ec5957758fc0a109dd99e3815.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "982af2c7ec41369ecc145e00086f956b",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "562a59b9a8e56c8e2389",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.87a7bd94.chunk.css"
+  },
+  {
+    "revision": "a87a26f8ceaa02dbb90f",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.976ec5a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.976ec5a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "562a59b9a8e56c8e2389",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.4241c8df.chunk.js"
+  },
+  {
+    "revision": "1d4efb2ec3f353f27b3e",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.f6a36cc7.js"
+  }
+]);
